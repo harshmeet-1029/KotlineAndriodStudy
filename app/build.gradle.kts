0209a355caf9a71aa041bfc87f6ce12c6518fa1c
Harshmeet Singh <harshmeetsingh010@gmail.com>
2025-03-16 18:29:15 +0530

    Images and Icons

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -56,4 +56,6 @@ dependencies {
     androidTestImplementation(libs.androidx.ui.test.junit4)
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
+    implementation(libs.coil.compose) //Hyphens (-) in keys are converted to dots (.) in libs.XXX notation ; coil-compose → libs.coil.compose
+
 }
--- app/src/main/java/com/example/kotlineandriodstudy/MainActivity.kt
+++ app/src/main/java/com/example/kotlineandriodstudy/MainActivity.kt
@@ -5,7 +5,9 @@ import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.horizontalScroll
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
@@ -16,25 +18,32 @@ import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.text.selection.DisableSelection
 import androidx.compose.foundation.text.selection.SelectionContainer
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Menu
+import androidx.compose.material.icons.outlined.Edit
 import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import coil.compose.rememberAsyncImagePainter
 import com.example.kotlineandriodstudy.ui.theme.WindSong
 
 
@@ -63,7 +72,9 @@ class MainActivity : ComponentActivity() {
 //            LazyColExample()
 //            BoxExample()
 //            MaterialDesignExample()
-
+//            AccessingImage()
+//            AccessingImageFromNetowrk()
+//            usingIcon()
             }
     }
 }
@@ -348,5 +359,38 @@ Scaffold(
 
 @Composable
 fun AccessingImage(){
+    Image(painter = painterResource(R.drawable.a), contentDescription = null, modifier = Modifier
+        .size(500.dp)
+        .border(2.dp, color = Color.Red,CircleShape)
+        .clip(shape = CircleShape)
+        .background(color = Color.Blue),
+        contentScale = ContentScale.Crop // Add the scaling to content
+    )
+}
+
+@Composable
+fun AccessingImageFromNetowrk(){
+    Image(painter = rememberAsyncImagePainter(
+        "https://getwallpapers.com/wallpaper/full/a/0/7/701514-gorgerous-iu-wallpaper-3840x2160-xiaomi.jpg"
+    ), contentDescription = null, modifier = Modifier
+        .size(500.dp)
+        .border(2.dp, color = Color.Red,CircleShape)
+        .clip(shape = CircleShape)
+        .background(color = Color.Blue),
+        contentScale = ContentScale.Crop // Add the scaling to content
+    )
+}
+
+@Composable
+fun usingIcon(){
+Icon(
+    Icons.Outlined.Edit, contentDescription = "Edit icon",
+    modifier = Modifier.size(20.dp),
+    tint = Color.Red
+)
+}
+
+@Composable
+fun CardExample(){
 
 }
